72e84318275fa3d829978a8844a948b9477779b5
PranavThe <64806092+PranavThe@users.noreply.github.com>
2025-01-13 07:35:54 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/secondgame/MainActivity.java
+++ app/src/main/java/com/example/secondgame/MainActivity.java
@@ -4,20 +4,33 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+//Main activity is the entry point to the application
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        Window window = getWindow();
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-        setContentView(new Game(this));
-    }
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       EdgeToEdge.enable(this);
+       setContentView(R.layout.activity_main);
+
+
+       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+           Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+           v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+           return insets;
+       });
+
+
+       //Set content view to game, so that objects in the game class can be rendered to the screen
+
+
+       setContentView(new Game(this));
+   }
 }
