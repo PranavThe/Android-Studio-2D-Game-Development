10217c289d84a674120e113303df03b9129ca348
Preston Masigla <pmasigla@students.monroe.k12.nj.us>
2025-01-13 08:19:50 -0500

    Preston9

---
+++ app/src/main/java/com/example/secondgame/Game.java
@@ -0,0 +1,97 @@
+package com.example.secondgame;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.core.content.ContextCompat;
+
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private final Player player;
+    private final Joystick joystick;
+    private GameLoop gameLoop;
+    private Context context;
+
+    public Game(Context context) {
+        super(context);
+
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        this.context = context;
+        gameLoop = new GameLoop(this, surfaceHolder);
+        joystick = new Joystick(275,700,70,40);
+        player = new Player(getContext(), 500, 500, 30);
+
+        setFocusable(true);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if(joystick.isPressed((double) event.getX(), (double) event.getY())){
+                    joystick.setIsPressed(true);
+                }
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                if(joystick.getIsPressed()){
+                    joystick.setActuator((double) event.getX(), (double) event.getY());
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                joystick.setIsPressed(false);
+                joystick.resetActuator();
+                return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) { gameLoop.startLoop(); }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {}
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+
+        joystick.draw(canvas);
+        player.draw(canvas);
+    }
+
+    public void drawUPS(Canvas canvas){
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.green);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS: " + averageUPS, 75, 50, paint);
+
+    }
+
+    public void drawFPS(Canvas canvas){
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.green);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("FPS: " + averageFPS, 75, 100, paint);
+
+    }
+
+    public void update() {
+        joystick.update();
+        player.update(joystick);
+    }
+}
---
+++ app/src/main/java/com/example/secondgame/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.secondgame;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(new Game(this));
+    }
+}
